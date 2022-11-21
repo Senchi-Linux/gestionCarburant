16b5efe90042f277cddca59e62da56fdc557d5f2
soumia Ing <sousoux-17@live.fr>
2022-11-21 10:50:43 +0100

    modify url

--- public/js/script.js
+++ public/js/script.js
@@ -30,7 +30,7 @@ function validerSup(params){
 
 
 $(document).ready(function () {
-  let url_variable="http://127.0.0.1:8000/";
+  let url_variable="https://app-gestion-carburant.herokuapp.com/";
   let monthNames = ["Janvier", "Février", "Mars", "Avril", "Mai","Juin","Juillet", "Août", "Septembre", "Octobre", "Novembre","Décembre"];
 
     $.ajaxSetup({
