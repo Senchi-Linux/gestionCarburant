60f73206483b3a064bb14e570df872029c006f8e
soumia Ing <sousoux-17@live.fr>
2022-11-22 15:49:31 +0100

    test 3

--- app/Http/Controllers/StatistiqueController.php
+++ app/Http/Controllers/StatistiqueController.php
@@ -152,7 +152,7 @@ class StatistiqueController extends Controller
             $tableValue[$value->indicem]=$value->compteur_year;
             array_push($tableMonths,$value->indicem);
         }
-return $tableValue;
+return $tableMonths;
         for($i=1; $i<=12; $i++){
             $date = Carbon::createFromFormat('m', $i);
             $monthName = $date->format('M');
